c6a8b400cc59c68546cd69761a1808b0901b841a
IsYet1 <Don@IsYet.com>
2016-09-08 16:35:39 -0500

    Added logger folder and index.js.

--- Module1/test.js
+++ Module1/test.js
@@ -8,15 +8,18 @@ var x = {
 console.log(x.name + " " + x.birthplace);
 
 var msgs =              require("./msgs.js");
-var msgsFunc =          require("./msgsFunc.js");
-var msgsConstructor =   require("./msgsConstructor.js");
 
 console.log(msgs.first);
 console.log(msgs.second);
 console.log(msgs.third);
 
+var msgsFunc =          require("./msgsFunc.js");
 console.log(msgsFunc("Add this to the output"));
 
+var msgsConstructor =   require("./msgsConstructor.js");
 var msgsCtor = new msgsConstructor("Constructor did this");
 console.log(msgsCtor.first);
 
+var logger = require("./logger");
+var loggerObj = new logger.log("Logger did this");
+//console.log();
