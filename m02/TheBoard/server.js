a63831b1e33a3427e97deeae891a6a94314ae727
IsYet1 <Don@IsYet.com>
2016-09-09 18:08:46 -0500

    Getting a respones.

--- m02/TheBoard/server.js
+++ m02/TheBoard/server.js
@@ -1,6 +1,8 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+var server = http.createServer(function (req, res) {
+    console.log(req.url);
+});
+
+server.listen(3000);
+console.log('Listening on 3000');
