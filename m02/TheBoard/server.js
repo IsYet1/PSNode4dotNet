2d2966d7991604c981c5313cc8dcbf7b752ebbc8
IsYet1 <Don@IsYet.com>
2016-09-10 15:50:33 -0500

    Add Jade package.

--- m02/TheBoard/server.js
+++ m02/TheBoard/server.js
@@ -2,8 +2,7 @@
 var express = require("express");
 var app = express();
 
-
-var server = http.createServer(app);
+app.set("view engine", "jade");
 
 app.get("/", function (req, res) {
     res.send("<html><body><h1>" + "Express 1509" + "</h1></body></html>");
@@ -11,8 +10,10 @@ app.get("/", function (req, res) {
 
 app.get("/api/users", function (req, res) {
     res.set("Content-Type", "application/json");
-    res.send({ name: "Shawn", isValid: true, group: "Admin" });
+    res.send({ name: "Don", isValid: true, group: "Admin" });
 });
+
+var server = http.createServer(app);
 
 server.listen(3000);
 console.log('Listening on 3000');
