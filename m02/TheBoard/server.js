af6dd9c163aaddcdfe0e6fb5d6a9212dd29b3551
IsYet1 <Don@IsYet.com>
2016-09-10 15:15:50 -0500

    Add api/users

--- m02/TheBoard/server.js
+++ m02/TheBoard/server.js
@@ -6,7 +6,12 @@ var app = express();
 var server = http.createServer(app);
 
 app.get("/", function (req, res) {
-    res.send("<html><body><h1>" + "Express" + "</h1></body></html>");
+    res.send("<html><body><h1>" + "Express 1509" + "</h1></body></html>");
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Shawn", isValid: true, group: "Admin" });
 });
 
 server.listen(3000);
