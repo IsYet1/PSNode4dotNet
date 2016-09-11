cc22c336137e79147b8e56b26a9d9c65bce713ec
IsYet1 <Don@IsYet.com>
2016-09-10 21:14:28 -0500

    Checkin before finishing data service

--- m02/TheBoard/controllers/homeController.js
+++ m02/TheBoard/controllers/homeController.js
@@ -1,4 +1,7 @@
 ﻿(function (homeController) {
+
+    var data = require("../data");
+
     homeController.init = function(app){
         app.get("/", function (req, res) {
 
@@ -6,7 +9,10 @@
             //res.render("jade/index", { title: "Express + Jade", moreText: "With layout" });
             //res.render("ejs/index", { title: "Express + EJS", moreText: "With layout" });
 
-            res.render("index", { title: "Express + Vash + Controller", moreText: "With layout" });
+            data.getNoteCategories(function (err, results) {
+                res.render("index", { title: "Express + Vash + Controller", moreText: "With layout", error: err, categories: results });
+            });
+
 
         });
     };
