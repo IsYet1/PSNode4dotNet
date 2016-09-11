1bb8a3e0ce3bf69014cb1321244bde340a5eb71b
IsYet1 <Don@IsYet.com>
2016-09-10 20:49:21 -0500

    Add back the commented out view engine lines
    Just so that I have them in the code.

--- m02/TheBoard/controllers/homeController.js
+++ m02/TheBoard/controllers/homeController.js
@@ -2,6 +2,10 @@
     homeController.init = function(app){
         app.get("/", function (req, res) {
 
+            //res.send("<html><body><h1>" + "Express 1509" + "</h1></body></html>");
+            //res.render("jade/index", { title: "Express + Jade", moreText: "With layout" });
+            //res.render("ejs/index", { title: "Express + EJS", moreText: "With layout" });
+
             res.render("index", { title: "Express + Vash + Controller", moreText: "With layout" });
 
         });
