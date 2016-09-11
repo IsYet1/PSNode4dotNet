5826b02c22245d30850926baabe10425306ce01e
IsYet1 <Don@IsYet.com>
2016-09-10 21:14:05 -0500

    Add data service components. Not tested yet.
    Haven't run any of this yet. Want to checkin to have a baseline.

---
+++ m02/TheBoard/data/index.js
@@ -0,0 +1,9 @@
+﻿(function (data) {
+
+    var seedData = require("./seedData");
+
+    //Async function. Will call next to tell the caller when it's done and if it was success or failure.
+    data.getNoteCategories = function (next) {
+        next(null, seedData.initialNodes);
+    };
+})(module.exports);
