4e40d27d7681dd42f499c688dc8f02c5ba8a861a
IsYet1 <Don@IsYet.com>
2016-09-12 11:28:00 -0500

    Added database.js
    Connecting to the DB only. Not saving anything yet.

--- m02/TheBoard/data/database.js
+++ m02/TheBoard/data/database.js
@@ -1,4 +1,26 @@
 ﻿//database.js
 (function (database) {
 
+    var mongodb = require("mongodb");
+    var mongoUrl = "mongodb://localhost:27017/theBoard";
+    var theDb = null;
+
+    database.getDb = function (next) {
+        if (!theDb) {
+            // Connect to the DB
+            mongodb.MongoClient.connect(mongoUrl, function (err, db) {
+                if (err) {
+                    next(err, null);
+                } else {
+                    theDb = {
+                        db: db
+                    };
+                    next(null, theDb);
+                }
+            });
+        } else {
+            next(null, theDb);
+        }
+    }
+
 })(module.exports);
