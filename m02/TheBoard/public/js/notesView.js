68a8d4ac20d1bcf213c98ef97f9ca98f97292da9
IsYet1 <Don@IsYet.com>
2016-09-27 13:01:35 -0500

    m5 Creating the Controller 01
    Add the controller in the NG Module.
    Before troubleshooting view engine issue.

--- m02/TheBoard/public/js/notesView.js
+++ m02/TheBoard/public/js/notesView.js
@@ -1,4 +1,28 @@
 ﻿//notesView.js
 (function (angular) {
     var theModule = angular.module("notesView", []);
+
+    theModule.controller("notesViewController",
+        ["$scope",
+            function ($scope) {
+                $scope.notes = [
+                    {
+                        note: "Hello World",
+                        color: "yellow",
+                        author: "Shawn W"
+                    },
+                    {
+                        note: "Hello World again",
+                        color: "blue",
+                        author: "Shawn W"
+                    },
+                    {
+                        note: "Hello World last",
+                        color: "green",
+                        author: "Shawn W"
+                    }
+                ];
+            }
+        ]);
+
 })(window.angular);
